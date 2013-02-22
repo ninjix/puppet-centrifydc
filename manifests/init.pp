b92c2cf6b3a4353b43f1db7d52fb5b3a987f8d04
Clayton Kramer <clayton.kramer@mail.cuny.edu>
2013-02-22 15:31:25 -0500

    big overhaul and modernization of the module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,113 +1,91 @@
+# Class: centrifydc
 #
-# Puppet manifest for Centrify Express
+# This module manages centrifydc
 #
+class centrifydc (
+  $ad_domain    = $domain,
+  $users_allow  = [],
+  $groups_allow = [],
+  $user_ignore  = [],
+  $group_ignore = []) {
+  include centrifydc::params
 
-class centrifydc {
+  $default_users_allow = $centrifydc::params::default_users_allow
+  $default_groups_allow = $centrifydc::params::default_groups_allow
+  $default_user_ignore = $centrifydc::params::default_user_ignore
+  $default_group_ignore = $centrifydc::params::default_group_ignore
 
-	# Install the latest Centrify Express client and join domain
-        package { centrifydc :
-                ensure => latest ,
-                notify => Exec["adjoin"]
-        }
-        
-		# This is only executed once when the package is installed.
-		# It requires "adjoin -w -P -n [new machine name] -u [domain administrator account] sps.cuny.edu" from the
-		# puppetmaster to pre-create the machine's account. Do this at the same time you sign the puppet certificate.
-		#
-		# This requires the $domain variable.
-        exec { "adjoin" :
-                path => "/usr/bin:/usr/sbin:/bin",
-		returns => 15,
-                command => "adjoin -w -S ${domain}",
-                refreshonly => true,
-                notify => Exec["addns"]
-        }
-        
-        # Update Active Directory DNS servers with host name
-        exec { "addns" :
-                path => "/usr/bin:/usr/sbin:/bin",
-		returns => 0,
-                command => "addns -U -m",
-                refresh => true,
-        }
-        
-        # Identify Ubuntu server and workstation machines by their kernel type
-        case $kernelrelease {
-	    /(server)$/: 
-	    { 
-	    	# Give the servers configuration that restricts logins to specific users and groups
-	    	file { "/etc/centrifydc/centrifydc.conf":
-			owner  => root,
-			group  => root,
-			mode   => 644,
-			source => "puppet:///centrifydc/server_centrifydc.conf",
-			require => Package["centrifydc"]
-		}
-		
-		# Additional users read from $users_allow array variable
-		file { "/etc/centrifydc/users.allow":
-			owner  => root,
-			group  => root,
-			mode   => 644,
-			content => template("centrifydc/server_users.allow.erb"),
-			require => Package["centrifydc"]
-		} 
-		
-		# Additional groups read from $groups_allow array variable
-		file { "/etc/centrifydc/groups.allow":
-			owner  => root,
-			group  => root,
-			mode   => 644,
-			content => template("centrifydc/server_groups.allow.erb"),
-			require => Package["centrifydc"]
-		} 
-	    } 
-	    default: 
-	    {	
-	    	# Use the default Centrify config
-	    	file { "/etc/centrifydc/centrifydc.conf":
-			owner  => root,
-			group  => root,
-			mode   => 644,
-			source => "puppet:///centrifydc/workstation_centrifydc.conf",
-			require => Package["centrifydc"]
-		}  
-	    } 
-	}
-        
-        
-	# Make sure service is running and is restarted if configuration files are updated
-        case $kernelrelease {
-	    /(server)$/: 
-	    {
-		service { centrifydc:
-		        ensure  => running,
-		        require => [
-		        	Package["centrifydc"],
-		        	File["/etc/centrifydc/centrifydc.conf"], 
-		        	File["/etc/centrifydc/users.allow"],
-		        	File["/etc/centrifydc/groups.allow"],
-		        ],
-		        subscribe => [ 
-		        	File["/etc/centrifydc/centrifydc.conf"], 
-		        	File["/etc/centrifydc/users.allow"], 
-		        	File["/etc/centrifydc/groups.allow"], 
-		        	Package["centrifydc"] 
-		        ]
-		}
-	   }
-	   default:
-	   {
-	   	service { centrifydc:
-		        ensure  => running,
-		        require => Package["centrifydc"],
-		        require => File["/etc/centrifydc/centrifydc.conf"],
-		        subscribe => [ 
-		        	File["/etc/centrifydc/centrifydc.conf"], 
-		        	Package["centrifydc"] 
-		        ]
-		}
-	   }
-	}
+  # Install the latest Centrify Express client and join domain
+  package { centrifydc:
+    ensure => latest,
+    notify => Exec["adjoin"]
+  }
+
+  # Join the domain
+  exec { "adjoin":
+    path    => $centrifydc::params::exec_path,
+    returns => 15,
+    command => "adjoin -w -S ${ad_domain}",
+    unless  => "adinfo -d | grep ${ad_domain}",
+    notify  => Exec["addns"]
+  }
+
+  # Update Active Directory DNS servers with host name
+  exec { "addns":
+    path        => $centrifydc::params::exec_path,
+    returns     => 0,
+    command     => 'addns -U -m',
+    refreshonly => true,
+  }
+
+  file {
+    '/etc/centrifydc/centrifydc.conf':
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      content => template('centrifydc/centrifydc.conf.erb'),
+      require => Package["centrifydc"];
+
+    '/etc/centrifydc/users.allow':
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      content => template("centrifydc/users.allow.erb"),
+      require => Package["centrifydc"];
+
+    '/etc/centrifydc/groups.allow':
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      content => template('centrifydc/groups.allow.erb'),
+      require => Package['centrifydc'];
+
+    '/etc/centrifydc/user.ignore':
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      content => template('centrifydc/user.ignore.erb'),
+      require => Package['centrifydc'];
+
+    '/etc/centrifydc/group.ignore':
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      content => template('centrifydc/group.ignore.erb'),
+      require => Package['centrifydc'];
+  }
+
+  service { 'centrifydc':
+    ensure     => running,
+    require    => Package['centrifydc'],
+    hasrestart => true,
+    subscribe  => [
+      File['/etc/centrifydc/centrifydc.conf'],
+      File['/etc/centrifydc/users.allow'],
+      File['/etc/centrifydc/groups.allow'],
+      File['/etc/centrifydc/user.ignore'],
+      File['/etc/centrifydc/group.ignore'],
+      Package['centrifydc']]
+  }
 
 }
